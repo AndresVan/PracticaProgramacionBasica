711e253b20256a6418c8802564890f30116d8ee5
Andrés_Vanegas_Castañeda <agvanegasc@gmail.com>
2023-05-24 16:29:04 -0500

    Revert "Creación de tablero de puntajes de Aciertos y Fallos y Diana ok"
    This reverts commit 9e2c83a9a5fceeae2ee9a46956ccc9d536ca9436.

--- V3/script.js
+++ V3/script.js
@@ -11,14 +11,7 @@ var maxX = 600;
 var maxY = 400;
 var radio = 10;
 var x = 0;
-var contadorAciertos = 0;
-var contadorFallos = 0;
-var play = 0;
-var mensaje;
 
-
-document.getElementById('aciertos').innerHTML = contadorAciertos;
-document.getElementById('fallos').innerHTML = contadorFallos;
 //--con disenharCircunferencia se especifíca el cómo crear un círculo -------------------
 function disenharCircunferencia(x,y,radio, color){
     pincel.fillStyle = color;
@@ -49,33 +42,11 @@ function limpiarPantalla(){
     pincel.clearRect(0,0,600,400);
 }
 pantalla.onclick = disparo; //Al dar click izq, se activa la función disparo
-//--- Con la función disparo se captura dentro de un rango la opción acertó o o acertó
 function disparo(evento){
     coorX = evento.pageX - pantalla.offsetLeft;
     coorY = evento.pageY - pantalla.offsetTop;
     console.log(coorX+ " Y " + coorY)
-    if(play < 3){
-        if ((movX + radio)>coorX && coorX > (movX - radio)&& (movY + radio)>coorY && coorY > (movY - radio) ){
-            alert ("Haz acertado!");
-            contadorAciertos++;
-            play++;
-            document.getElementById('aciertos').innerHTML = contadorAciertos;
-        }
-        else{
-            alert("Casi, pero no");
-            contadorFallos++;
-            play++;
-            document.getElementById("fallos").innerHTML = contadorFallos;
-        }
+    if ((movX + radio)>coorX && coorX > (movX - radio)&& (movY + radio)>coorY && coorY > (movY - radio) ){
+    alert ("Haz acertado!");
     }
-    else if(contadorAciertos>=contadorFallos){
-            mensaje = "arquero!";
-            alert("Tu puntería es de " + mensaje);
-            location.reload();
-        }else{
-            mensaje = "manco!";
-            alert("Fin del Juego, tu puntería es de " + mensaje);
-            location.reload();
-        }
-          
 }
